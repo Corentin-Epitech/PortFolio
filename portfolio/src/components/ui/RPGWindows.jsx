ead7848e76b02548875a69e1321ece28a0b19dea
Corentin <corentin.courtine@epitech.eu>
2026-09-17 11:20:22 +0200

    Add: RPGWindows first version

---
+++ portfolio/src/components/ui/RPGWindows.jsx
@@ -0,0 +1,21 @@
+import "./RPGWindows.css";
+
+function RPGWindow({ title, children, className = "" }) {
+    return (
+        <section className={`rpg-window ${className}`}>
+            {title && (
+                <div className="rpg-window__header">
+                    <h2 className="rpg-window__title">
+                        {title}
+                    </h2>
+                </div>
+            )}
+
+            <div className="rpg-window__content">
+                {children}
+            </div>
+        </section>
+    );
+}
+
+export default RPGWindow;
